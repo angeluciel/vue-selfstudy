e52323769455f7e73ea4e802022fec4ceb92c0fb
Luciel <jpmizidoro@gmail.com>
2024-07-25 17:49:08 -0300

    Header mostly finished
    Tried styling the header better, also created the "main header" on App.vue, will probably change it to a component later, but its good enough to remind me to do it later.
    Still need to style better the leftside of the header, I'll learn a bit about grid layout and do it again!

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,27 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <main>
     <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+      <div class="header__left">
+        <RouterLink to="/" class="material-icons">home</RouterLink>
+      </div>
+      <div class="header__right">
+        <RouterLink to="/about" class="material-icons">menu</RouterLink>
+      </div>
     </header>
 
     <RouterView />
   </main>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background: rgba(23, 21, 26, 0.3);
+  color: #6b6a6f;
+  padding: 1rem 1rem 0.5rem 1rem;
+  border-bottom: 2px solid red;
+}
+</style>
